aba03a8f0db59f2cad6d59fc55de61090e21b1f1
johngonzalez <johngonzalez@outlook.com>
2016-04-18 12:41:24 -0500

    Init clean project

--- client/main.js
+++ client/main.js
@@ -2,14 +2,14 @@ import {createApp} from 'mantra-core';
 import initContext from './configs/context';
 
 // modules
-import coreModule from './modules/core';
-import commentsModule from './modules/comments';
+// import coreModule from './modules/core';
+// import commentsModule from './modules/comments';
 
 // init context
 const context = initContext();
 
 // create app
 const app = createApp(context);
-app.loadModule(coreModule);
-app.loadModule(commentsModule);
+// app.loadModule(coreModule);
+// app.loadModule(commentsModule);
 app.init();
--- server/main.js
+++ server/main.js
@@ -1,7 +1,5 @@
-import publications from './publications';
-import methods from './methods';
-import addInitialData from './configs/initial_adds.js';
+// import publications from './publications';
+// import methods from './methods';
 
-publications();
-methods();
-addInitialData();
+// publications();
+// methods();
