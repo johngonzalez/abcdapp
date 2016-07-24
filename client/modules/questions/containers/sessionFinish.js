9870e76a250acc1008cf268d4d7ec2eb98a63564
johngonzalez <johngonzalez@outlook.com>
2016-07-23 19:47:13 -0500

    Corrected context in props

--- client/modules/questions/containers/sessionFinish.js
+++ client/modules/questions/containers/sessionFinish.js
@@ -1,14 +1,15 @@
 import {useDeps, composeWithTracker, composeAll} from 'mantra-core';
 import SessionFinish from '../components/sessionFinish';
 
-export const composer = ({LocalState, clearState, sessionId, code}, onData) => {
+export const composer = ({context, clearState, sessionId, code}, onData) => {
+  const {LocalState} = context();
   const error = LocalState.get('SESSION_FINISH_ERROR');
   onData(null, {sessionId, code, error});
   return clearState;
 };
 
 export const depsMapper = (context, actions) => (
-  {...context, ...actions.sessionFinish}
+  { context: () => context, ...actions.sessionFinish }
 );
 
 export default composeAll(
--- client/modules/questions/containers/sessionRegister.js
+++ client/modules/questions/containers/sessionRegister.js
@@ -1,17 +1,16 @@
 import {useDeps, composeWithTracker, composeAll} from 'mantra-core';
 import SessionRegister from '../components/sessionRegister.js';
 
-export const composer = ({LocalState, clearState}, onData) => {
+export const composer = ({context, clearState}, onData) => {
+  const {LocalState} = context();
   const error = LocalState.get('CODE_REGISTER_ERROR');
   // TODO: Better ui because appear two times the same error when render session component
   onData(null, {error});
-
-  // clearErrors when unmounting the component
   return clearState;
 };
 
 export const depsMapper = (context, actions) => (
-  { ...context, ...actions.sessionRegister }
+  { context: () => context, ...actions.sessionRegister }
 );
 
 export default composeAll(
