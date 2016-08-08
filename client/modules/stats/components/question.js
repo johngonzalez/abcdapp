78c1b59cdfa3e4944ea80af66bbfe4eabb62be12
johngonzalez <johngonzalez@outlook.com>
2016-08-08 06:48:20 -0500

    Added bar to stats

--- client/modules/stats/components/question.js
+++ client/modules/stats/components/question.js
@@ -1,19 +1,50 @@
 import React from 'react';
+import {VictoryChart, VictoryBar, VictoryAxis} from 'victory-chart';
+import {Col, Row} from 'react-bootstrap';
 
-const Question = ({question, responsesGroup}) => (
+const Question = ({question, ownSelected, responsesGroup}) => (
   <div>
     <p>Correcta: {String.fromCharCode(97 + question.response)}</p>
-    <hr />
-     {
-      responsesGroup.map((rg, i) => (
-        <div key={i}>{String.fromCharCode(97 + i)}: {rg.length}</div>
-      ))
+    {
+      ownSelected === undefined || ownSelected === -1 ?
+      <p>No respondiste esta pregunta</p> :
+      <p>Tu respuesta: {String.fromCharCode(97 + ownSelected)}</p>
     }
+    <hr />
+    <Row>
+      <Col xs={12} md={6} style={{float: 'none', margin: '0 auto'}}>
+        <VictoryChart height={280}>
+          <VictoryAxis dependentAxis
+            style={{
+              tickLabels: {fontSize: 24},
+              axis: {strokeWidth: 0},
+            }}
+
+          />
+          <VictoryBar horizontal
+            style={{
+              data: {width: 50, fill: '#3071a9'},
+              labels: {fontSize: 24}
+            }}
+            data = {
+              responsesGroup.reverse().map((rg, idx) => (
+                {
+                  x: String.fromCharCode(100 - idx),
+                  y: rg.length,
+                  label: rg.length,
+                }
+              ))
+            }
+          />
+        </VictoryChart>
+      </Col>
+    </Row>
   </div>
 );
 
 Question.propTypes = {
   question: React.PropTypes.object.isRequired,
+  ownSelected: React.PropTypes.number,
   responsesGroup: React.PropTypes.array.isRequired
 };
 
--- client/modules/stats/containers/question.js
+++ client/modules/stats/containers/question.js
@@ -1,20 +1,28 @@
 import {useDeps, composeWithTracker, composeAll} from 'mantra-core';
 import Question from '../components/question';
 import {propMerge, objToColl, sortByOr} from '../libs/helpers';
-import {values, prop, groupBy, pluck, map, compose} from 'ramda';
+import {propOr, find, propEq, values, prop, groupBy, pluck, map, compose} from 'ramda';
 
 export const composer = ({context, sessionId, questionId}, onData) => {
   const {Collections, Meteor} = context();
   const question = Collections.Questions.findOne(questionId);
   const responses = Collections.Responses.find({questionId}).fetch();
   const users = Meteor.users.find().fetch();
+  const userId = Meteor.userId();
 
+  // Helper function
   const merge = compose(
     values,
     propMerge('_id', 'owner')
   );
 
-  const responsesWithUsers = merge(users, responses);
+  // Helper function
+  const findByAndProp = compose(
+    propOr(-1, 'selected'),
+    find(propEq('owner', userId))
+  );
+
+  // Helper function
   const groupSelected = compose(
     sortByOr([], 'students', 'selected', [ '0', '1', '2', '3' ]),
     objToColl('selected', 'students'),
@@ -22,8 +30,13 @@ export const composer = ({context, sessionId, questionId}, onData) => {
     groupBy(prop('selected'))
   );
 
+  const responsesWithUsers = merge(users, responses);
+  console.log(responsesWithUsers);
+  const ownSelected = findByAndProp(responsesWithUsers);
+  console.log(ownSelected);
   const responsesGroup = groupSelected(responsesWithUsers);
-  onData(null, {question, responsesGroup});
+
+  onData(null, {question, ownSelected, responsesGroup});
 };
 
 export const depsMapper = context => ({context: () => context});
